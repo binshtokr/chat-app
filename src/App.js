be535f626f9c07b7385d47ec42d09888008d56c7
RB <roman@gmx.de>
2025-01-14 18:41:09 +0100

    removed unused use in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { use, useState } from "react";
+import React, { useState } from "react";
 import io from "socket.io-client";
 import "./App.css"; // Assuming you have a CSS file for styling
 
