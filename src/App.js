7b96fbb1ccfd9d726ecff6299f035a88cfe09484
RB <roman@gmx.de>
2025-01-15 19:11:17 +0100

    produrl

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 import "./App.css"; // Assuming you have a CSS file for styling
